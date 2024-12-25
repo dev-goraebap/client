d2f7c648b5d045808d6880355c62dedeb8493942
dev.goraebap <dev.goraebap@gmail.com>
2024-12-25 15:59:19 +0900

    Feat: 라우터 설정

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login', 
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('../pages/login').then(m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('../pages/register').then(m => m.RegisterPage)
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('../pages/profile').then(m => m.ProfilePage)
+  },
+];
