9cfc50efa74a54c5a2958d72a3cc2537c5b1476a
dev.goraebap <dev.goraebap@gmail.com>
2024-12-25 15:58:45 +0900

    Feat: 프로필 페이지 추가

---
+++ src/pages/profile/profile.page.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { tap } from "rxjs";
+import { AuthService, ProfileResultDTO } from "src/entities/user";
+
+@Component({
+    selector: 'profile-page',
+    imports: [
+        ReactiveFormsModule
+    ],
+    template: `
+    <div>
+        프로필 페이지
+    </div>
+    `,
+})
+export class ProfilePage {
+
+    private readonly authService: AuthService = inject(AuthService);
+
+    constructor() {
+        this.authService.getProfile().pipe(
+            tap((res: ProfileResultDTO) => {
+                console.log(res);
+            })
+        ).subscribe();
+    }
+}
