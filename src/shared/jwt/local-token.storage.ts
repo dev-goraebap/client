1819898b29d2fefe06e3c9e0c921b66fe796f074
dev.goraebap <dev.goraebap@gmail.com>
2024-12-29 16:36:20 +0900

    Feat: update jwt interceptor

--- src/shared/jwt/local-token.storage.ts
+++ src/shared/jwt/local-token.storage.ts
@@ -1,28 +1,36 @@
+import { Injectable } from "@angular/core";
 import { Preferences } from "@capacitor/preferences";
 
+@Injectable({
+    providedIn: 'root'
+})
 export class LocalTokenStorage {
-
-    /**
-     * 싱글톤 패턴을 위한 프로퍼티
-     */
-    private static instance: LocalTokenStorage;
 
     private accessToken: string | null = null;
     private expiresIn: number | null = null;
 
     private refreshTokenKey: string = 'RT';
 
-    private constructor() { }
-
-    static getInstance(): LocalTokenStorage {
-        if (!this.instance) {
-            this.instance = new LocalTokenStorage();
-        }
-        return this.instance;
-    }
-
     getAccessToken(): string | null {
         return this.accessToken;
+    }
+
+    async getRefreshToken(): Promise<string | null> {
+        const result = await Preferences.get({ key: this.refreshTokenKey });
+        return result.value ?? null;
+    }
+
+    isExpiringSoon(): boolean {
+        if (!this.expiresIn) {
+            return true;
+        }
+        const expiresIn = this.expiresIn * 1000;
+        const fiveMinutes = 5 * 60 * 1000; // 5분
+
+        const timeUntilExpiry = expiresIn - Date.now();
+
+        // 만료되었거나 5분 이하로 남은 경우
+        return timeUntilExpiry <= fiveMinutes;
     }
 
     async store(accessToken: string, expiresIn: number, refreshToken: string): Promise<void> {
@@ -33,5 +41,11 @@ export class LocalTokenStorage {
         console.log('accessToken', this.accessToken);
         console.log('expiresIn', this.expiresIn);
         console.log('refreshToken', await Preferences.get({ key: this.refreshTokenKey }));
+    }
+
+    async clear() {
+        this.accessToken = null;
+        this.expiresIn = null;
+        await Preferences.remove({ key: this.refreshTokenKey });
     }
 }
